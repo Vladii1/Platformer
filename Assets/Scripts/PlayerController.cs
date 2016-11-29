f9b95c062aa1fbb82c30bb9b5334bbfaed049a16
Marcin Szypura <szypura.marcin@gmail.com>
2016-11-29 14:31:26 +0100

    Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float jumpHeight;
+	Rigidbody2D rigidbody2D;
+	// Use this for initialization
+	void Start () {
+		rigidbody2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown(KeyCode.Space)) 
+		{
+			rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, jumpHeight);
+		}
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			rigidbody2D.velocity = new Vector2 (speed, rigidbody2D.velocity.y);
+		}
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			rigidbody2D.velocity = new Vector2 (-speed, rigidbody2D.velocity.y);
+		}
+
+		/*
+		if (Input.GetAxis ("Horizontal")!=0|| Input.GetAxis("Vertical")!=0) 
+		{
+			rigidbody2D.velocity = new Vector2 (Input.GetAxis ("Horizontal") * speed, 0);
+			rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, Input.GetAxisRaw ("Vertical") * jumpHeight);
+		}
+	*/
+	}
+}
